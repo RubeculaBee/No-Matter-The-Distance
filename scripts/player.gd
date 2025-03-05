ccf29443d2c7936a03f26c7861fbffcb8fd44b41
Robin Lane <183433480+rubeculabee@users.noreply.github.com>
2025-03-04 23:50:05 -0800

    Constant Adjustments

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 640
+const SPEED = 64
 var direction
 
 func _physics_process(delta: float) -> void:
 	direction = Input.get_axis("p1_left","p1_right")
 	
 	if(!is_on_floor()):
-		velocity.y += get_gravity() * delta
+		velocity += get_gravity() * delta
 	
-	velocity.x = direction * SPEED * delta
+	velocity.x = direction * SPEED
 	
 	move_and_slide()
