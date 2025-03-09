66d1642a9ac879dfde628d99ce3ee6d2e7940e28
Robin Lane <183433480+rubeculabee@users.noreply.github.com>
2025-03-08 23:20:42 -0700

    Added player 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,13 +5,14 @@ const JUMP_SPEED = 400;
 var direction
 
 @onready var _sprite = $AnimatedSprite2D # Get the attached animated sprite
+@onready var ID = name.get_slice("_", 1) # ID equals 1 for player_1 and 2 for player_2
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	# direction = -1 or 1 if left or right respectively is pressed
-	direction = Input.get_axis("p1_left","p1_right") 
+	direction = Input.get_axis("p%s_left" % ID,"p%s_right" % ID) 
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -22,7 +23,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if is_on_floor(): _sprite.play("default")
 	
-	if is_on_floor() and Input.is_action_just_pressed("p1_up"):
+	if is_on_floor() and Input.is_action_just_pressed("p%s_up" % ID):
 		velocity.y -= JUMP_SPEED
 		_sprite.play("jump")
 	
