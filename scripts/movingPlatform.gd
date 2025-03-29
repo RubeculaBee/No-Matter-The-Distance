33acefdce4dd5f4c6f781c46a848f005ff699e8a
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-03-28 20:44:42 -0700

    Added "active" toggle to moving platforms

--- scripts/movingPlatform.gd
+++ scripts/movingPlatform.gd
@@ -2,6 +2,7 @@ extends AnimatableBody2D
 
 @export var move_distance = Vector2()                    # how many tiles the platform should move
 @export var SPEED = 1.                                   # speed is in tiles per second
+@export var active = true                                # whether the platform moves by default
 @onready var start_position = position                   # where the platform starts
 @onready var move_direction = move_distance.normalized() # direction the platform will move
 var time = 0                                             # seconds since the platform was created
@@ -21,11 +22,13 @@ func _ready() -> void:
 	##print("My Pos: ", position, " | End Pos: ", end_node.position)
 
 func _physics_process(delta: float) -> void:
-	time += delta
-	# platform moves in the direction of the end point at the given speed
-	position += move_direction * SPEED * delta
-	#if the amount of time elapsed reaches the time it takes for the platform to move to the end
-	if time >= sqrt(pow(move_distance.x,2) + pow(move_distance.y,2))/SPEED:
-		#revers the direction and reset the timer
-		move_direction *= -1
-		time = 0
+	if active:
+		# track active time
+		time += delta
+		# platform moves in the direction of the end point at the given speed
+		position += move_direction * SPEED * delta
+		#if the amount of time elapsed reaches the time it takes for the platform to move to the end
+		if time >= sqrt(pow(move_distance.x,2) + pow(move_distance.y,2))/SPEED:
+			#revers the direction and reset the timer
+			move_direction *= -1
+			time = 0
