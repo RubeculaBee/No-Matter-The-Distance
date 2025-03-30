5405464fff56dbc82954c583e633e7516d497460
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-03-29 22:05:18 -0700

    Buttons now toggle moving platforms

--- scripts/movingPlatform.gd
+++ scripts/movingPlatform.gd
@@ -5,6 +5,7 @@ extends AnimatableBody2D
 @export var active = true                                # whether the platform moves by default
 @onready var start_position = position                   # where the platform starts
 @onready var move_direction = move_distance.normalized() # direction the platform will move
+@onready var button = find_child("Button", false)        # the button associated with the platform, if one exists
 var time = 0                                             # seconds since the platform was created
 
 # debug square is a black square
@@ -14,6 +15,10 @@ func _ready() -> void:
 	# 1 tile is 32 pixels
 	move_distance *= 32
 	SPEED *= 32
+	
+	if not button == null:
+		button.pressed.connect(_on_button_pressed)
+		button.released.connect(_on_button_released)
 	
 	# debug square shows where the end position is
 	##add_child(debug_square)
@@ -32,3 +37,9 @@ func _physics_process(delta: float) -> void:
 			#revers the direction and reset the timer
 			move_direction *= -1
 			time = 0
+
+func _on_button_pressed():
+	active = not active
+
+func _on_button_released():
+	active = not active
