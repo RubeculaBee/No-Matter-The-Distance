c06994bc41ca703d236e0ed3a66c65eef9a5d54b
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-03-30 23:36:49 -0700

    Added sky background with moving clouds to level

--- scripts/background.gd
+++ scripts/background.gd
@@ -1,1 +1,16 @@
 extends ColorRect
+
+# cloud object
+var cloud = preload("res://scenes/game objects/cloud.tscn")
+
+func _ready() -> void:
+	for n in 15:
+		var new_cloud = cloud.instantiate()
+		add_child(new_cloud)
+		new_cloud.exited.connect(_on_cloud_exited)
+
+func _on_cloud_exited():
+	var new_cloud = cloud.instantiate()
+	add_child(new_cloud)
+	new_cloud.position.x = 1280 + new_cloud.size.x/2
+	new_cloud.exited.connect(_on_cloud_exited)
--- scripts/cloud.gd
+++ scripts/cloud.gd
@@ -1,1 +1,24 @@
 extends Sprite2D
+
+var speed = 1.0
+var size = Vector2()
+
+signal exited
+
+func _ready() -> void:
+	scale.x = randf_range(0.5, 2)
+	scale.y = scale.x
+	size = Vector2(64 * scale.x, 32 * scale.y)
+	position = Vector2(randi_range(0, 1280), randi_range(0, 385))
+	speed = scale.x
+	
+	exited.connect(_on_cloud_exited)
+
+func _process(delta: float) -> void:
+	position.x -= 32 * speed * delta
+	
+	if position.x < 0 - size.x/2:
+		exited.emit()
+
+func _on_cloud_exited():
+	queue_free()
