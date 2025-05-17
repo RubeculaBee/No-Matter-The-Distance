19d5da62455004931c4b3ed067c264937fcb95e0
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-05-17 16:58:48 -0700

    Commented clodu script and background script

--- scripts/background.gd
+++ scripts/background.gd
@@ -4,12 +4,14 @@ extends ColorRect
 var cloud = preload("res://scenes/game objects/cloud.tscn")
 
 func _ready() -> void:
+	# Create 15 clouds and add them to the scene
 	for n in 15:
 		var new_cloud = cloud.instantiate()
 		add_child(new_cloud)
 		new_cloud.exited.connect(_on_cloud_exited)
 
 func _on_cloud_exited():
+	#when a cloud leaves, create a new one and put it outside the screen
 	var new_cloud = cloud.instantiate()
 	add_child(new_cloud)
 	new_cloud.position.x = 1280 + new_cloud.size.x/2
--- scripts/cloud.gd
+++ scripts/cloud.gd
@@ -1,24 +1,29 @@
 extends Sprite2D
 
-var speed = 1.0
-var size = Vector2()
+var speed = 1.0      # how many tiles per second the cloud moves
+var size = Vector2() # the clouds dimensions in pixels
 
 signal exited
 
 func _ready() -> void:
+	# When a cloud is created, give it a random scale and position
 	scale.x = randf_range(0.5, 2)
 	scale.y = scale.x
 	size = Vector2(64 * scale.x, 32 * scale.y)
 	position = Vector2(randi_range(0, 1280), randi_range(0, 385))
+	# It's speed is based on it's scale (smaller clouds move slower, as if they were far away)
 	speed = scale.x
 	
 	exited.connect(_on_cloud_exited)
 
 func _process(delta: float) -> void:
+	# Cosntantly move left
 	position.x -= 32 * speed * delta
 	
+	# When leaving the screen, emit the exit signal
 	if position.x < 0 - size.x/2:
 		exited.emit()
 
 func _on_cloud_exited():
+	# When a cloud leaves, remvoe it
 	queue_free()
