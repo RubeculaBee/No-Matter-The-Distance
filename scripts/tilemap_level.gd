d3f5cceee844da4a5d534dd6036748a35d113911
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-05-17 17:09:45 -0700

    Tilemap now gets the tiles each player is standing on each frame

--- scripts/tilemap_level.gd
+++ scripts/tilemap_level.gd
@@ -8,24 +8,16 @@ var atlas_coords  = [[Vector2(-1,-1), Vector2(-1,-1)],[Vector2(-1,-1), Vector2(-
 @onready var player_2 = get_parent().get_node("Zone P2/player_2")
 
 func _process(_delta: float) -> void:
+	get_player_tiles(player_1)
+	get_player_tiles(player_2)
+	
 	# if either player presses down when on the ground, attempt to fall through
 	if player_1.is_on_floor() and Input.is_action_just_pressed("p1_down"):
 		fall_through(player_1)
-	
 	if player_2.is_on_floor() and Input.is_action_just_pressed("p2_down"):
 		fall_through(player_2)
 
 func fall_through(player: CharacterBody2D):
-	# Get the two tiles the player is standing on
-	# (12 pixels (half player width) left/right and 48 pixels (1&1/2 blocks) down, devided by 32 (tile width))
-	tile_coords[player.ID - 1][0] = floor((player.position + Vector2(-12, 48))/32)
-	tile_coords[player.ID - 1][1] = floor((player.position + Vector2( 12, 48))/32)
-	
-	# Get the atlas coordinates of the two tiles
-	# "atlas coordinates" is the tiles location in the tile set
-	atlas_coords[player.ID - 1][0] = get_cell_atlas_coords(tile_coords[player.ID - 1][0])
-	atlas_coords[player.ID - 1][1] = get_cell_atlas_coords(tile_coords[player.ID - 1][1])
-	
 	# All one-way platforms are in the 4th row (3rd y-index) of the tile set
 	# if the two tiles are one-way platforms (at the 3rd y-index), 
 	# set the tile to it's first alt tile (which has no collision)
@@ -40,6 +32,17 @@ func fall_through(player: CharacterBody2D):
 	match player.ID:
 		1: oneway_timer.timeout.connect(_on_p1_oneway_timer_timeout)
 		2: oneway_timer.timeout.connect(_on_p2_oneway_timer_timeout)
+
+func get_player_tiles(player: CharacterBody2D):
+	# Get the two tiles the player is standing on
+	# (12 pixels (half player width) left/right and 48 pixels (1&1/2 blocks) down, devided by 32 (tile width))
+	tile_coords[player.ID - 1][0] = floor((player.position + Vector2(-12, 48))/32)
+	tile_coords[player.ID - 1][1] = floor((player.position + Vector2( 12, 48))/32)
+	
+	# Get the atlas coordinates of the two tiles
+	# "atlas coordinates" is the tiles location in the tile set
+	atlas_coords[player.ID - 1][0] = get_cell_atlas_coords(tile_coords[player.ID - 1][0])
+	atlas_coords[player.ID - 1][1] = get_cell_atlas_coords(tile_coords[player.ID - 1][1])
 
 # when the oneway platform timer runs out (after 0.05 seconds)
 # set the two tiles back to their default version (which has collision)
